a928e3cc6e616a07218f2c5c79f2f46dc152cae9
AMOAmandaMoumen <123954436+AMOAmandaMoumen@users.noreply.github.com>
2023-01-30 16:05:24 +0000

    Update BlueStyleProfile.js

--- BlueStyleProfile.js
+++ BlueStyleProfile.js
@@ -2,34 +2,10 @@
 	AMO CONSULTANCY - Custom Theme - 01/2023
 */
 
-const HEADER_VIEW_NAME = 'VIEW_HEADER';
-const HEADER_LOGO_VIEW_NAME = 'VIEW_HEADER_Logo';
-const HEADER_TRANSLATION_VIEW_NAME = 'VIEW_HEADER_Translator';
+$('body').addClass('amoconsultancy');
+$('form').addClass('amoconsultancy');
+$('.runtime-content').addClass('amoconsultancy');
+$('.runtime-form').addClass('amoconsultancy');
+$('[name*="VIEW_HEADER"]').insertBefore(".tabs-top");
 
-$(document).ready(function() {
-	enableTheme();
-	render();
-});
-
-enableTheme = () => {
-	$('body').addClass('amoconsultancy');
-	$('form').addClass('amoconsultancy');
-	$('.runtime-content').addClass('amoconsultancy');
-	$('.runtime-form').addClass('amoconsultancy');
-}
-
-render = () => {
-	renderBeforeTabs();
-	renderHeader();
-}
-
-renderBeforeTabs = () => {
-    	$('[name*="' + HEADER_VIEW_NAME + '"]').insertBefore(".tabs-top");
-}
-
-renderHeader = () => {
-    	$('div[name*="' + HEADER_VIEW_NAME + '"]').closest('.view').addClass('header');
-		$('div[name="' + HEADER_LOGO_VIEW_NAME + '"]').closest('.view').addClass('logo');
-		$('div[name="' + HEADER_TRANSLATION_VIEW_NAME + '"]').closest('.view').addClass('translator');
-}
 
