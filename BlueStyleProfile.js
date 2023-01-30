a0ab570fca006c36acae7e31d3ff72c3c1b2a26c
AMOAmandaMoumen <123954436+AMOAmandaMoumen@users.noreply.github.com>
2023-01-30 15:09:03 +0000

    Update BlueStyleProfile.js

--- BlueStyleProfile.js
+++ BlueStyleProfile.js
@@ -1,1 +1,29 @@
-$('[name*="VIEW_TOP"]').insertBefore(".tabs-top");
+const BANNER_VIEW_NAME = 'BANNER';
+const REQUEST_LIST_VIEW_NAME = 'VIEW_REQUEST_LIST';
+const HEADER_VIEW_NAME = 'HEADER';
+
+$(document).ready(function() {
+    enableTheme();
+    render();
+});
+
+enableTheme = () => {
+    $('body').addClass('roullier');
+    $('form').addClass('roullier');
+    $('.runtime-content').addClass('roullier');
+    $('.runtime-form').addClass('roullier');
+}
+
+render = () => {
+    renderHeader();
+    renderListView();
+}
+
+renderHeader = () => {
+    $('div[name="' + HEADER_VIEW_NAME + '"]').closest('.view').addClass('header');
+    $('div[name*="' + HEADER_VIEW_NAME + '"]').insertBefore(".tabs-top");
+}
+
+renderListView = () => {
+    $('div[name="' + REQUEST_LIST_VIEW_NAME + '"]').closest('.view').addClass('requests');
+}
