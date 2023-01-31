1d49945988c97a41d3f2fe0705b4ffdeb0bf3cb9
AMOAmandaMoumen <123954436+AMOAmandaMoumen@users.noreply.github.com>
2023-01-31 10:37:37 +0000

    Update BlueStyleProfile.js

--- BlueStyleProfile.js
+++ BlueStyleProfile.js
@@ -6,6 +6,6 @@ $('body').addClass('amoconsultancy');
 $('form').addClass('amoconsultancy');
 $('.runtime-content').addClass('amoconsultancy');
 $('.runtime-form').addClass('amoconsultancy');
-$('[name*="VIEW_HEADER"]').insertBefore(".tabs-top");
 $('[name*="VIEW_HEADER"]').closest('.tabs-top').addClass('tabsinheader');
+$('[name*="VIEW_HEADER"]').insertBefore(".runtime-form");
 $('[name*="NO_BORDER"]').closest('.innerpanel').addClass('noborder');
